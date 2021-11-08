2b5eb8bc04127514d39cc43a75074f4f0d05dd95
Clemence Gazier <clemencegazier@MacBook-Pro-de-Clemence-2.local>
2021-11-08 17:12:46 +0100

    add img

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,9 +1,26 @@
 import React, {useState} from 'react'
+import './Slider.css'
+import dataSlider from './dataSlider'
 
 export default function Slider() {
+
+    const [slideAnim, setSlideAnim] = useState({
+        index: 1,
+        inProgress: false
+    })
+
     return (
-        <div>
-            
+        <div className="container-slider">
+            {dataSlider.map((obj, index) => {
+                return(
+                    <div
+                        key={obj.id}
+                        className={slideAnim.index === index + 1 ? "slide active-anim" : "slide"}
+                        >
+                            <img src={`/Imgs/img${index + 1}.jpg`} alt="" />
+                    </div>
+                )
+            })}
         </div>
     )
 }
